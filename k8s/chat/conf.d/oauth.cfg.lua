ede8b265062c7e326ad9cdc8e9c1b901e3a9230e
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2026-03-28 17:05:19 +0100

    added support /metrics
    external name equal to internal name app.numworx

---
+++ k8s/chat/conf.d/oauth.cfg.lua
@@ -0,0 +1,7 @@
+authentication = "oauthbearer"
+
+oauth_client_id = "13f8e9cc8928b3409822"
+oauth_client_secret = "983161fd3ah608ea7ef35382668aad1927463978"
+oauth_url = "http://app.numworx/dwo/oauth2/verify.jsp?client_id={{oauth_client_id}}&token={{password}}"
+
+
---
+++ k8s/metrics/grafana/grafana.sh
@@ -0,0 +1,1 @@
+kubectl -n numworx port-forward pod/grafana 3000:3000
